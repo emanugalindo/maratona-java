8d0b61af54e8160ce23c20b6ad124f45fa2b7224
emanugalindo <emanuel.galindo410@gmail.com>
2025-05-05 15:29:56 -0300

    Aula 56: POO - Modificador de acesso private,get e set pt 03

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,10 +5,11 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funciona
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "José";
-        funcionario.idade = 38;
-        funcionario.salarios = new double[]{2500, 2000, 3000};
+        funcionario.setNome("José");
+        funcionario.setIdade(38);
+        funcionario.setSalarios(new double[]{2500, 2000, 3000});
 
         funcionario.imprime();
+        System.out.println("Média " + funcionario.getMedia());
     }
 }

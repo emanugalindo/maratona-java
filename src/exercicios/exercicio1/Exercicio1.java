aa9fd29ae6b930a8daec667c204e13922efc59d5
emanugalindo <emanuel.galindo410@gmail.com>
2025-08-12 09:35:29 -0300

    feat: adição Exercício 1

---
+++ src/exercicios/exercicio1/Exercicio1.java
@@ -0,0 +1,23 @@
+package exercicios.exercicio1;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        /*
+        Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma
+        entre A e B é mostre se a soma é menor que C.
+         */
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, soma;
+        System.out.println("Digite o valor de a: ");
+        a = sc.nextInt();
+        System.out.println("Digite o valor de b: ");
+        b = sc.nextInt();
+        System.out.println("Digite o valor de c: ");
+        c = sc.nextInt();
+        soma = a + b;
+        System.out.println("Soma: " + soma);
+        System.out.println(soma < c ? "A soma é menor que o valor de C" : "A soma é maior ou igual ao valor de C");
+    }
+}

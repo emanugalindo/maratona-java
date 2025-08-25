859b33606255df3b2ab636d91aa219214fd637c0
emanugalindo <emanuel.galindo410@gmail.com>
2025-08-25 17:56:25 -0300

    Aula 139: Classes Utilitárias - IO pt 02 - FileWriter

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriterTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferedReader
+public class FileWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("Escrevendo no arquivo\nEscrevendo na próxima linha\n");
+            fw.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}

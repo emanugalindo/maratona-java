71e9dc499f09871e4ae6c38a9d468cb1453b5246
emanugalindo <emanuel.galindo410@gmail.com>
2025-08-26 14:23:53 -0300

    Aula 142: Classes Utilitárias - IO pt 05 - BufferedReader

--- src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("Escrevendo no arquivo");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("Escrevendo no arquivo");
+            bw.newLine();
+            bw.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }

7b697a49052a36bd680ff0188fbcd301d1fe7577
emanugalindo <emanuel.galindo410@gmail.com>
2025-12-10 09:59:05 -0300

    Aula 164: Coleções pt 04 - hashCode pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -16,11 +16,20 @@ public class Smartphone {
     // para x diferente de null, x.equals(null) tem que retornar false.
     @Override
     public boolean equals(Object obj) {
-        if(obj == null) return false;
-        if(this == obj) return true;
-        if(this.getClass() != obj.getClass()) return false;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
         Smartphone smartphone = (Smartphone) obj;
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    // se x.equals(y) == true, y.hashCode() == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashCode() != x.hashcCode(), x.equals(y) deverá ser false.
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
     public String getSerialNumber() {

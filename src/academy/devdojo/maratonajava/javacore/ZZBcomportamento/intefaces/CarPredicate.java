c1f854c6050c4a9a2f5e7f53fb3deb0db358e371
emanugalindo <emanuel.galindo410@gmail.com>
2025-12-24 10:41:10 -0300

    Aula 195: Lambdas pt 01 - Predicate

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/intefaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/intefaces/CarPredicate.java
@@ -2,6 +2,10 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.intefaces;
 
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anônimas, funções, conciso
     boolean test(Car car);
+    //(parametro) -> <expressão>
+    //(Car car) - > car.getColor.equals("green");
 }

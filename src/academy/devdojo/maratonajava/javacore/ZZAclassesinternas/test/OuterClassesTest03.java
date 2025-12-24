b20ff4a342aa917281c46f7452b048652cf04924
emanugalindo <emanuel.galindo410@gmail.com>
2025-12-23 22:30:01 -0300

    Aula 192: Classes Internas pt 04 - Classes aninhadas estáticas

---
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest03.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest03 {
+    private String name = "Emanuel";
+    static class Nested {
+        private String lastName = "Galindo";
+        void print(){
+            System.out.println(new OuterClassesTest03().name + " " + lastName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Nested nested = new Nested();
+        nested.print();
+    }
+}

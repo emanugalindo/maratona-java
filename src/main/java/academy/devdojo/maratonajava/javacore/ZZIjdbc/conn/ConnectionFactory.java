cc9dd24db00bd936976a13090a36b00b020bdb25
emanugalindo <emanuel.galindo410@gmail.com>
2026-01-11 18:39:31 -0300

    Aula 257: JDBC pt 06 - Inserindo dados com Statement

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -6,16 +6,10 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     //java.sql = Connection, Statement, ResultSet, DriverManager
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/anime_store";
         String username = "emanuel";
         String password = "162131";
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println(connection);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return DriverManager.getConnection(url, username, password);
     }
 }

2046567c6484760babb740f0785bbdf5094099cf
emanugalindo <emanuel.galindo410@gmail.com>
2026-01-13 09:41:00 -0300

    Aula 269: JDBC pt 18 - PreparedStatement pt 02

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -44,6 +44,24 @@ public class ProducerRepository {
         }
     }
 
+    public static void updatePreparedStatement(Producer producer) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = preparedStatementUpdate(conn, producer)) {
+            int rowsAffected = ps.executeUpdate();
+            log.info("Update producer '{}' from the database, rows affected '{}'", producer.getId(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    private static PreparedStatement preparedStatementUpdate(Connection conn, Producer producer) throws SQLException {
+        String sql = "UPDATE producer SET name = ? WHERE (id = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        ps.setInt(2, producer.getId());
+        return ps;
+    }
+
     public static List<Producer> findAll() {
         log.info("Finding all Producers");
         return findByName("");
@@ -72,10 +90,9 @@ public class ProducerRepository {
 
     public static List<Producer> findByNamePreparedStatement(String name) {
         log.info("Finding Producers by name");
-        String sql = "SELECT * FROM producer WHERE name LIKE ?;";
         List<Producer> producers = new ArrayList<>();
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = createPreparedStatement(conn, sql, name);
+             PreparedStatement ps = preparedStatementFindByName(conn, name);
              ResultSet rs = ps.executeQuery();) {
             while (rs.next()) {
                 Producer producer = Producer.builder()
@@ -90,9 +107,10 @@ public class ProducerRepository {
         return producers;
     }
 
-    private static PreparedStatement createPreparedStatement(Connection conn, String sql, String name) throws SQLException {
+    private static PreparedStatement preparedStatementFindByName(Connection conn, String name) throws SQLException {
+        String sql = "SELECT * FROM producer WHERE name LIKE ?;";
         PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, "%" + name + "%");
+        ps.setString(1, String.format("%%%s%%", name));
         return ps;
     }
 
